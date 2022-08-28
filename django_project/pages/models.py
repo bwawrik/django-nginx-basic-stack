60791a4cb97eea1585566117f8d36dc579f9dcb6
Boris Wawrik <bwawrik@yahoo.com>
2022-08-27 23:03:08 -0400

    Update models.py

--- django_project/pages/models.py
+++ django_project/pages/models.py
@@ -1,3 +1,11 @@
+## if you are going to make changes here,  you 
+## need to migrate the database; version control on db
+## migration will only work for installed apps
+## > source .venv/bin/activate
+## > python3 manage.py makemigrations
+## > python3 manage.py migrate
+
+
 from django.db import models
 
 # Create your models here.
