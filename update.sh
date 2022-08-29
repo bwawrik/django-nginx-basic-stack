b2aa7e49ffd0456e0ab39882a6ff1422ba3c954f
Boris Wawrik <bwawrik@yahoo.com>
2022-08-28 22:06:28 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,7 @@
 # dev2_django_gunicorn_1 is the name of the docker containter; dev2 is teh folder name
-# moving the dockerignore to a backup; then update the docker, but don't voerwrite the sqlite3 database
+# save the db.sqlite3 
 
-mv .dockerignore .dockerignore_backup
-echo "db.sqlite3" > .dockerignore
+docker cp dev2_django_gunicorn_1:/app/db.sqlite3 ./django_project/
+docker cp ./django_project/. dev2_django_gunicorn_1:/app/
 docker restart dev2_django_gunicorn_1
-rm .dockerignore
-mv .dockerignore_backup .dockerignore
 
